841265ea2bc325c4ea0e2a7bff02ad8d7bf5dcdd
SomanathKambar <kambarsomanath@gmail.com>
2025-01-13 12:35:01 +0530

    Initial commit :Adding base spring initializer code

---
+++ src/main/kotlin/com/somanath/first_spring_boot_kt/FirstSpringBootKtApplication.kt
@@ -0,0 +1,11 @@
+package com.somanath.first_spring_boot_kt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FirstSpringBootKtApplication
+
+fun main(args: Array<String>) {
+	runApplication<FirstSpringBootKtApplication>(*args)
+}
---
+++ src/test/kotlin/com/somanath/first_spring_boot_kt/FirstSpringBootKtApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.somanath.first_spring_boot_kt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FirstSpringBootKtApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
